4082ab33de389bdd062a7ec5d2ae5894a12e7904
sho-takeuchi <moonlight2248@yahoo.co.jp>
2020-03-11 18:36:25 +0900

    見本通りに完成した

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -5,7 +5,6 @@ $(function(){
     $(".result").hide();   
     //問題数を取得
     var qNum = $("ul li").length;
-    
     if( $("ul li input:checked").length < qNum ){
       //全てチェックしていなかったらアラートを出す
       alert("未回答の問題があります");
@@ -16,18 +15,18 @@ $(function(){
         typeBNum = $(".typeB:checked").length,
         typeCNum = $(".typeC:checked").length,
         typeDNum = $(".typeD:checked").length;
-      if( typeANum > typeBNum + typeCNum + typeDNum / 3 ) {
+      if( typeANum > (typeBNum + typeCNum + typeDNum - 3)) {
         //もしもAの方が多かったらAタイプを表示
         $(".ResultA").fadeIn();
-      } else if( typeBNum > typeANum + typeCNum + typeDNum / 3 ) {
+      } else if( typeBNum > (typeANum + typeCNum + typeDNum - 3)) {
         //もしもBの方が多かったらBタイプを表示
         $(".ResultB").fadeIn();
       }
-      else if( typeCNum > typeANum + typeBNum + typeDNum / 3 ) {
+      else if( typeCNum >(typeANum + typeBNum + typeDNum - 3) ) {
         //もしもBの方が多かったらBタイプを表示
         $(".ResultC").fadeIn();
       }
-      else if( typeDNum > typeANum + typeBNum + typeCNum / 3 ) {
+      else if( typeDNum > (typeANum + typeBNum + typeCNum - 3) ) {
         //もしもBの方が多かったらBタイプを表示
         $(".ResultD").fadeIn();
       }
