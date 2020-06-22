328b956a95960b286e98eaa849b8d8672cb33c4d
sho-takeuchi <moonlight2248@yahoo.co.jp>
2020-06-22 09:59:15 +0900

    修正

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -16,17 +16,18 @@ $(function(){
             typeCNum = $(".typeC:checked").length,
             typeDNum = $(".typeD:checked").length,
             typeENum = $(".typeD:checked").length;
-        if( typeANum >= typeBNum && typeANum >= typeCNum && typeANum >= typeDNum && typeANum >= typeENum) {
-            $(".ResultA").fadeIn();
-        } else if( typeBNum >= typeANum && typeBNum >= typeCNum && typeBNum >= typeDNum && typeBNum >= typeENum) {
-            $(".ResultB").fadeIn();
-        } else if( typeCNum >= typeANum && typeCNum >= typeBNum && typeCNum >= typeDNum && typeCNum >= typeENum) {
-            $(".ResultC").fadeIn();
-        } else if( typeDNum >= typeBNum && typeDNum >= typeCNum && typeDNum >= typeANum && typeDNum >= typeENum) {
-            $(".ResultD").fadeIn();
-        } else if( typeENum >= typeBNum && typeENum >= typeCNum && typeENum >= typeANum && typeENum >= typeDNum) {
-            $(".ResultE").fadeIn();
-      }
+            const max = Math.max(typeANum, typeBNum, typeCNum, typeDNum, typeENum);
+            if( typeANum >= max) {
+                $(".ResultA").fadeIn();
+            } else if( typeBNum >= max) {
+                $(".ResultB").fadeIn();
+            } else if( typeCNum >= max) {
+                $(".ResultC").fadeIn();
+            } else if( typeDNum >= max) {
+                $(".ResultD").fadeIn();
+            } else if( typeENum >= max) {
+                $(".ResultE").fadeIn();
+            }
     }
   });
 });
